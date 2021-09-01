151b4b9f3b06116b6f475a63896d44d9d0d7863a
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-09-01 15:10:12 -0500

    Clase 1 Reto

--- reto_clase1.js
+++ reto_clase1.js
@@ -8,4 +8,5 @@ function imc(peso, altura){
 let peso = 65; //kg
 let altura = 1.78; //m
 
-console.log(imc(peso,altura)); //kg/m2
+console.log(imc(peso,altura));  //kg/m2
+//console.log(typeof imc(peso,altura)); //number
