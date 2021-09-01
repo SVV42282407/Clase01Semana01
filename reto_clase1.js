e8974306629a8db975933d23b0aaa20c5007e321
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-09-01 15:07:27 -0500

    Clase 1 Reto

---
+++ reto_clase1.js
@@ -0,0 +1,11 @@
+//Cálculo de IMC: IMC = peso(kg)/altura^2 (m2)
+
+function imc(peso, altura){
+    imc = peso / (altura^2);
+    return imc;
+}
+
+let peso = 65; //kg
+let altura = 1.78; //m
+
+console.log(imc(peso,altura)); //kg/m2
