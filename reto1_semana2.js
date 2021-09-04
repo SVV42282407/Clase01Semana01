3998cfe8e5cc62573c82e287646168592a263fa6
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-09-04 10:55:29 -0500

    Reto 1- Semana 2: Josué Rodríguez

---
+++ reto1_semana2.js
@@ -0,0 +1,41 @@
+const dataList = [{
+    "idioma":"Español",
+    "texto":"Bienvenido al curso de React Native en PachaQtec",
+    "id_idioma":1
+ },
+ {
+    "idioma":"Ingles",
+    "texto":"Welcome to the React Native course at PachaQtec",
+    "id_idioma":2
+ },
+ {
+    "idioma":"Francés",
+    "texto":"Bienvenue dans le cours React Native chez PachaQtec",
+    "id_idioma":3
+ },
+ {
+    "idioma":"Italiano",
+    "texto":"Benvenuti al corso React Native presso PachaQtec",
+    "id_idioma":4
+ },
+ {
+    "idioma":"Portugués",
+    "texto":"Bem-vindo ao curso React Native na PachaQtec",
+    "id_idioma":5
+ }
+]
+
+const traducir = (data, text, idiomaDestino) => {
+    var r = data.some(i => i.texto.includes(text));
+    if (r){
+        for (el in data){
+            if (idiomaDestino == data[el].idioma){
+                return data[el].texto;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(traducir(dataList, 'Bienvenue dans le cours React Native chez PachaQtec', 'Italiano'));
+
