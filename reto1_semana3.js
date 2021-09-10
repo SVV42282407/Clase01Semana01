0da9528c0cbecb9b35ff35dd2e3dde8b170bf124
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-09-10 18:53:31 -0500

    Reto 1 Semana 3 - Josué Rodríguez

---
+++ reto1_semana3.js
@@ -0,0 +1,21 @@
+//item a)
+for (i=0; i<=20; i++){
+    if((i % 2) == 0){
+        console.log(i + " es par");
+    }
+}
+
+for (i=0; i<=10; i++){
+    console.log(i + " * 9 = " + i*9);
+}
+
+for (i=1; i<=10; i++){
+    console.log("Tabla del " + i + ":");
+    console.log("======================");
+    for(j=1; j<=10; j++){
+        console.log(i + " * " + j + " = " + i*j);
+    }
+    
+}
+
+//item b)
