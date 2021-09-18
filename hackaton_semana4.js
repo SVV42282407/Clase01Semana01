58a28ae25c57f19f1df792dcaf12ca9dfc4c2756
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-09-18 10:56:09 -0500

    Hackaton Semana 4: Josué Rodríguez

--- hackaton_semana4.js
+++ hackaton_semana4.js
@@ -51,5 +51,54 @@ async function loadJsonAsync(url){
 //loadJsonAsync('no-such-user.json'); // Error: 404
 
 //Ejercicio 4
+class HttpError extends Error {
+    constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+    }
+}
+
+function loadJson(url) {
+    return fetch(url)
+    .then(response => { 
+    if (response.status == 200) {
+        return response.json(); 
+    } else {
+        throw new HttpError(response); }
+    });
+}
+    // Ask for a user name until github returns a valid userfunction 
+function demoGithubUser() { 
+    let name = prompt("Enter a name?", "iliakan"); 
+    return loadJson(`https://api.github.com/users/${name}`) 
+        .then(user => { alert(`Full name: ${user.name}.`); return user; }) 
+        .catch(err => { 
+            if (err instanceof HttpError && err.response.status == 404) { 
+                alert("No such user, please reenter.");
+                return demoGithubUser(); 
+            } else { 
+                throw err; 
+            } 
+        });
+}
+
+demoGithubUser();
+
+async function demoGithubUserAsync(){
+    let name = prompt("Enter a name?", "iliakan"); 
+    try {
+        await loadJson(`https://api.github.com/users/${name}`);
+    } catch(err) {
+        if (err instanceof HttpError && err.response.status == 404) { 
+            alert("No such user, please reenter.");
+            return demoGithubUserAsync(); 
+        } else { 
+            throw err; 
+        } 
+    }
+}
+
+//Ejercicio 5
 
     
