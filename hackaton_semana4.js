9a6977f466e39b9d21db182db2204454633d5839
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-09-18 10:41:14 -0500

    Hackaton Semana 4: Josué Rodríguez

--- hackaton_semana4.js
+++ hackaton_semana4.js
@@ -15,3 +15,41 @@ function delay(ms) {
 }
 
 delay(3000).then(() => alert('runs after 3 seconds'));
+
+//Ejercicio 2
+//promise.then(f1).catch(f2);
+//Versus:
+//promise.then(f1, f2);
+
+//Rpta: Es posible usar cualquiera, sin embargo es recomendable usar catch por el manejo de errores de una manera más clara.
+
+//Ejercicio 3:
+//fetch se puede usar en el browser
+
+function loadJson(url) {
+    return fetch(url)
+     .then(response => {
+        if (response.status == 200) { 
+            return response.json();
+        } else { 
+            throw new Error(response.status);
+        }
+    });
+}
+
+//loadJson('no-such-user.json').catch(err => console.log(err)); // Error: 404
+
+async function loadJsonAsync(url){
+    let response = await fetch(url);
+    if (response.status == 200) { 
+        return response.json();
+    } else { 
+        throw new Error(response.status);
+    }
+}
+
+//loadJsonAsync('no-such-user.json'); // Error: 404
+
+//Ejercicio 4
+
+    
